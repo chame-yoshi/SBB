589b7f1629df2b082d67d0b5c27a10d550199d3f
chame-yoshi <58153770+chame-yoshi@users.noreply.github.com>
2019-11-29 12:01:54 +0900

    Delete tileset.tsx

--- assets/tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.0" name="tileset" tilewidth="32" tileheight="32" tilecount="140" columns="20">
- <image source="images/tileset.png" width="640" height="241"/>
-</tileset>
